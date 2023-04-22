e4773fdc22116fc7d07075833cb01cadb7db5774
nik <108171566+xdgreat@users.noreply.github.com>
2023-04-22 21:02:47 +1200

    Add files via upload

--- Envoy-Fiji/script/script.js
+++ Envoy-Fiji/script/script.js
@@ -21,50 +21,22 @@ learn.addEventListener("mouseleave", () => {
   link.classList.remove("fa-shake");
 });
 
-const container = document.getElementById("top-rated-container");
-const electronicsEl = Categories.Electronics.length;
+// let i = -1;
 
-for (let i = 0; i < electronicsEl; i++) {
-  const product = document.createElement("div");
-  product.classList.add("products");
-  const image = document.createElement("div");
-  image.classList.add("product-image");
-  const description_container = document.createElement("div");
-  description_container.classList.add("product-container-description");
-  const name = document.createElement("p");
-  name.classList.add("product-name");
-  const hr = document.createElement("hr");
-  const hr2 = document.createElement("hr");
-  const price = document.createElement("p");
-  price.classList.add("product-price");
-  const description = document.createElement("p");
-  description.classList.add("product-description");
-  const addToCart = document.createElement("button");
-  addToCart.classList.add("button-56");
-  addToCart.textContent = "Add To Cart";
-  addToCart.style.cursor = "pointer";
+// window.setInterval(() => {
+//   if (i < Categories.Electronics.length - 1) {
+//   } else {
+//     i = -1;
+//   }
+//   const activePicture = document.getElementById("hero");
+//   i++;
+//   activePicture.style.backgroundImage = Categories.Electronics[i].image;
+//   console.log(Categories.Electronics.length - 1, i);
+// }, 2000);
 
-  image.style.backgroundImage = Categories.Electronics[i].image;
-  name.textContent = Categories.Electronics[i].name;
-  price.textContent = Categories.Electronics[i].price;
-  description.textContent = Categories.Electronics[i].description;
-
-  addToCart.style.display = "none"
-  description.style.display = "none";
-
-  description_container.append(name, hr, price, hr2, description);
-  product.append(image, description_container, addToCart);
-  container.append(product);
-
-  console.log(Categories.Electronics[i].name);
-
-  product.addEventListener("mouseenter", () => {
-    description.style.display = "inherit";
-    addToCart.style.display = "inherit";
+const t = document
+  .getElementById("hero-section")
+  .addEventListener("click", (e) => {
+    console.log(e.offsetX, e.offsetY);
   });
 
-  product.addEventListener("mouseleave", () => {
-    description.style.display = "none";
-    addToCart.style.display = "none";
-  });
-}
